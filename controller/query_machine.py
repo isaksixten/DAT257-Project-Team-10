bd6aa24ef7edcb7f897dee6b76abe1c2c60a55ff
Isak Sixten <isak.sixten@gmail.com>
2024-04-08 12:34:36 +0200

    two primitive queries for database

---
+++ controller/query_machine.py
@@ -0,0 +1,29 @@
+import psycopg2
+
+class QueryMachine:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            host = "localhost",
+            user = "postgres",
+            password = "postgres")
+        self.conn.autocommit = True
+
+    def fetch_location(self, id):
+        with self.conn.cursor() as cur:
+            sql = """ SELECT * FROM Locations WHERE Locations.id = %s"""
+            cur.execute(sql, (id,))
+            res = cur.fetchone()
+            if res:
+                return res
+            else:
+                return "Location does not exist"
+            
+    def add_location(self, id, name, description, latitude, longitude):
+        try:
+            with self.conn.cursor() as cur:
+                sql = """INSERT INTO Locations VALUES (%s, %s, %s, %s, %s)"""
+                cur.execute(sql, (id, name, description, latitude, longitude))
+
+        except psycopg2.Error as e:
+            message = repr(e)
+            return "failed: " + message
