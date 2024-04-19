defa8a79a36193585026fbed448f0f279ec54653
melkerr1 <melkerr@chalmers.se>
2024-04-19 10:31:36 +0200

    Added functionality to insert API farms in db

--- backend/APItest.py
+++ backend/APItest.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from query_machine import QueryMachine
 
 API_KEY = ''    #PUSHA EJ TILL GIT
 
@@ -8,6 +9,8 @@ base_url_nearby = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
 
 # Place Details API
 base_url_details = 'https://maps.googleapis.com/maps/api/place/details/json'
+
+query=QueryMachine()
 
 # Funkar bara på sverige nu eftersom man behöver andra keywords för att hitta bongårdar i andra länder. Alltså det respektive landets översättning av "bondgård" exempelvis.
 def local_farms_sweden(longitude, latitude):
@@ -18,9 +21,9 @@ def local_farms_sweden(longitude, latitude):
         'keyword': 'bondgård'  
     }
 
-    include_fields = ["name", "business_status", "formatted_address", "formatted_phone_number", "geometry", "rating", "url", "website", "wheelchair_accessible_entrance"]
-
-    all_farms = {}
+    include_fields = ["name", "business_status", "formatted_address", "international_phone_number", "geometry", "rating", "url", "website", "wheelchair_accessible_entrance","open_now"]
+    #periods? (öppettider)
+    all_farms = {}  #Onödig, bara under develop-fas
 
     response_nearby = requests.get(base_url_nearby, params=params_nearby)
 
@@ -45,12 +48,17 @@ def local_farms_sweden(longitude, latitude):
                 included_farm_details = {field: farm_details.get(field, None) for field in include_fields}
                     
                 farm_name = included_farm_details.get('name', 'Unnamed Farm')
-                all_farms[farm_name] = included_farm_details
+                all_farms[farm_name] = included_farm_details    #onödigt bara under develop-fasen
+
+                import random       #Behöver ändras till något rimligt
+                query.add_location(random.randint(0,100),included_farm_details['name'],'empty',included_farm_details['geometry']['location']['lat'],
+                included_farm_details['geometry']['location']['lng'],)
             else:
                 print('Error:', response_details.status_code)
+                
     else:
         print('Error:', response_nearby.status_code)
 
     with open('farm_data_temp.json', 'w', encoding='utf-8') as json_file:
-        json.dump(all_farms_by_name, json_file, indent=4, ensure_ascii=False)
+        json.dump(all_farms, json_file, indent=4, ensure_ascii=False)
 local_farms_sweden(59.334591, 18.063240)
